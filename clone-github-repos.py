2d32ba216accdf1d6d042646975af5ad89bd47ed
Gurjit Singh <g.s@outlook.in>
2022-05-30 16:37:45 +0530

    bugfix

--- clone-github-repos.py
+++ clone-github-repos.py
@@ -1,16 +1,21 @@
+from argparse import ArgumentParser
 from json import loads
 from os import chdir, mkdir
 from os.path import exists, expanduser, join
-from subprocess import PIPE, run
 from shutil import rmtree
-from sys import argv
+from subprocess import PIPE, run
+
+# from pprint import PrettyPrinter
 
 
-try:
-    if argv[1] == "-p":
-        isPrivate = True
-except:
-    isPrivate = False
+def parseArgs():
+    parser = ArgumentParser()
+    parser.add_argument("-p", "--private", action="store_true")
+
+    return parser.parse_args()
+
+
+pargs = parseArgs()
 
 
 def download(url):
@@ -18,13 +23,17 @@ def download(url):
     run(["git", "clone", url])
 
 
-downloadDir = "github-private-repos" if isPrivate else "github-public-repos"
+downloadDir = "github-private-repos" if pargs.private else "github-public-repos"
 
 downloadPath = join(expanduser("~"), "Downloads", downloadDir)
 
-getRepos = run(["hub", "api", "/user/repos"], check=True, stdout=PIPE)
+getRepos = run(
+    ["gh", "api", "/user/repos?per_page=100&page=1"], check=True, stdout=PIPE
+)
+# &visibility=private
 
-reposAll = getRepos.stdout.decode().replace("'", "").strip()
+reposAll = getRepos.stdout.decode().strip()
+# .replace("'", "")
 
 if exists(downloadPath):
     rmtree(downloadPath)
@@ -34,9 +43,13 @@ chdir(downloadPath)
 
 repos = loads(reposAll)
 
+# pprint = PrettyPrinter().pprint
+
 for repo in repos:
-    if isPrivate:
+    if pargs.private:
         if repo["private"]:
+            # pprint(repo["name"])
+            # pprint(repo["private"])
             download(repo["ssh_url"])
     else:
         if not repo["fork"]:
